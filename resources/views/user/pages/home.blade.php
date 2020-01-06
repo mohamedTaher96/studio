8770986500c2b3199ce82ca43fb3553e9315c14f
MTaher <goman2013@gmail.com>
2020-01-06 23:32:35 +0200

    dsa

--- resources/views/user/pages/home.blade.php
+++ resources/views/user/pages/home.blade.php
@@ -1,12 +1,12 @@
-@extends('user/layout/arabicLayout')
-@section('header')
+{{-- @extends('user/layout/arabicLayout')
+@section('header') --}}
     {{-- <div id="owl-demo" class="owl-carousel owl-theme">
         @foreach ($covers as $cover)
             <div class="item"><img src="{{asset('images/covers/'.$cover->src)}}" alt="..."></div>
         @endforeach
     </div> --}}
         
-    <div class="hidden">
+    {{-- <div class="hidden">
         <a class="btn owl-btn next"><span class="fa fa-angle-right"></span></a>
         <a class="btn owl-btn prev"><span class="fa fa-angle-left"></span></a>
     </div>
@@ -20,4 +20,16 @@
         }
 
     </script>
-@endsection
+@endsection --}}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>asddsdddd</h1>
+</body>
+</html>
